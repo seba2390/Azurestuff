01bfb0a79bfc3ed007ede4dc5618850d55268cb6
Sebastian Yde Madsen <75695915+seba2390@users.noreply.github.com>
2023-12-04 14:12:00 +0100

    fg

--- qiskit_main.py
+++ qiskit_main.py
@@ -60,17 +60,17 @@ def main():
             N_xx_yy_angles += __layers__ * (N - 2)
         if w_z_phase:
             N_xx_yy_angles += N * __layers__
-        theta_i = np.random.normal(loc=0, scale=1, size=N_xx_yy_angles)
+        # theta_i = np.random.normal(loc=0, scale=1, size=N_xx_yy_angles)
+        theta_i = np.random.uniform(-0.05, 0.05, N_xx_yy_angles)
 
-        iteration_dicts = []
+        # iteration_dicts = []
 
-        def callback_function(x):
-            iteration_dicts.append(ansatz.get_state_probabilities(angles=x, flip_states=False))
+        """def callback_function(x):
+            iteration_dicts.append(ansatz.get_state_probabilities(angles=x, flip_states=False))"""
 
         res = sc.optimize.minimize(fun=ansatz.get_cost, x0=theta_i,
                                    method=_available_methods_[_method_idx_],
-                                   options={'disp': False, 'maxiter': __max_iter__},
-                                   callback=callback_function)
+                                   options={'disp': False, 'maxiter': __max_iter__})
         _dict_ = ansatz.get_state_probabilities(angles=res.x, flip_states=False)
         Final_circuit_sample_states = np.array([[int(bit) for bit in key] for key in list(_dict_.keys())], dtype=int)
         Final_circuit_sample_probabilities = np.array([_dict_[key] for key in list(_dict_.keys())], dtype=np.float64)
@@ -93,17 +93,17 @@ def main():
                     'Optimizer_nfev': res.nfev,
                     'Optimizer_maxfev': __max_iter__,
                     'Rng_seed': __seed__,
-                    'status': res.status,
-                    'iteration_cost': [normalized_cost(result=it,
-                                                       QUBO_matrix=Q,
-                                                       QUBO_offset=offset,
-                                                       max_cost=max_cost,
-                                                       min_cost=min_cost) for it in iteration_dicts]}
+                    'status': res.status}
+        # 'iteration_cost': [normalized_cost(result=it,
+        #                                    QUBO_matrix=Q,
+        #                                    QUBO_offset=offset,
+        #                                    max_cost=max_cost,
+        #                                    min_cost=min_cost) for it in iteration_dicts]}"""
         result.append(TO_STORE)
         return result
 
     alpha = 0.001
-    N_seeds = 100
+    N_seeds = 42
     max_iter = 5000
     min_layers, max_layers = 5, 5
     N_max = 15
@@ -115,7 +115,7 @@ def main():
             for seed in np.random.randint(low=0, high=2 ** 31, size=N_seeds):
                 datapoints.append((N, k, layers, max_iter, seed, alpha))
 
-    N_jobs = 60
+    N_jobs = 14
     r = Parallel(n_jobs=N_jobs, verbose=51, backend='loky')(delayed(simulate)(datapoint) for datapoint in datapoints)
 
     for run in r:

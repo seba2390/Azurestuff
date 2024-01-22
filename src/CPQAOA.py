91def3987cce57a89b49264be877699e1d28c320
Sebastian Yde Madsen <madsen3008@gmail.com>
2024-01-22 15:36:53 +0100

    update

--- src/CPQAOA.py
+++ src/CPQAOA.py
@@ -6,18 +6,108 @@ from qiskit import QuantumCircuit, Aer, execute
 from qiskit.circuit import Parameter
 from qiskit.circuit.library import PauliEvolutionGate
 from qiskit.quantum_info import Operator
-from qiskit.opflow import X, Y
-from qiskit.primitives import Estimator
-from qiskit_algorithms.gradients import ParamShiftEstimatorGradient, LinCombEstimatorGradient, FiniteDiffEstimatorGradient
 import numpy as np
 from scipy.linalg import expm
 import torch
 
-from src.TorchQcircuit import TorchQcircuit
 from src.Tools import get_qiskit_H
 from src.Tools import qubo_cost, string_to_array, create_operator, operator_expectation, get_generator
 from src.Grid import Grid
 from src.Chain import Chain
+
+
+def create_Rxx_matrix(n_qubits: int, qubit_1: int, qubit_2: int, angle: torch.Tensor) -> torch.Tensor:
+    # Check if qubit indices are within the range
+    if qubit_1 >= n_qubits or qubit_2 >= n_qubits or qubit_1 < 0 or qubit_2 < 0:
+        raise ValueError("Qubit indices are out of bounds.")
+    elif qubit_1 == qubit_2:
+        raise ValueError("Qubit indices are equal (the should be different)")
+    # Pauli X gate
+    X = torch.tensor([[0, 1], [1, 0]], dtype=torch.complex128)
+    # Identity matrix for other qubits
+    I = torch.eye(2, dtype=torch.complex128)
+    # Create the full tensor product for the R_xx gate
+    if qubit_1 == 0 or qubit_2 == 0:
+        gate = X
+    else:
+        gate = I
+    for i in range(1, n_qubits):
+        if i == qubit_1 or i == qubit_2:
+            gate = torch.kron(gate, X)
+        else:
+            gate = torch.kron(gate, I)
+    # R_xx gate matrix
+    R_xx = torch.matrix_exp(-1j * angle / 2 * gate)
+    return R_xx
+
+
+def create_Rz_matrix(n_qubits: int, qubit: int, angle: torch.Tensor) -> torch.Tensor:
+    # Check if qubit indices are within the range
+    if qubit >= n_qubits or qubit < 0:
+        raise ValueError("Qubit indices are out of bounds.")
+    # Pauli Z gate
+    Z = torch.tensor([[1, 0], [0, -1]], dtype=torch.complex128)
+    # Identity matrix for other qubits
+    I = torch.eye(2, dtype=torch.complex128)
+    # Create the full tensor product for the R_z gate
+    if qubit:
+        gate = Z
+    else:
+        gate = I
+    for i in range(1, n_qubits):
+        if i == qubit:
+            gate = torch.kron(gate, Z)
+        else:
+            gate = torch.kron(gate, I)
+    # R_z gate matrix
+    R_z = torch.matrix_exp(-1j * angle / 2 * gate)
+    return R_z
+
+
+def create_Ryy_matrix(n_qubits: int, qubit_1: int, qubit_2: int, angle: torch.Tensor) -> torch.Tensor:
+    # Check if qubit indices are within the range
+    if qubit_1 >= n_qubits or qubit_2 >= n_qubits or qubit_1 < 0 or qubit_2 < 0:
+        raise ValueError("Qubit indices are out of bounds.")
+    elif qubit_1 == qubit_2:
+        raise ValueError("Qubit indices are equal (the should be different)")
+    # Pauli Y gate
+    Y = torch.tensor([[0, -1j], [1j, 0]], dtype=torch.complex128)
+    # Identity matrix for other qubits
+    I = torch.eye(2, dtype=torch.complex128)
+    # Create the full tensor product for the R_yy gate
+    if qubit_1 == 0 or qubit_2 == 0:
+        gate = Y
+    else:
+        gate = I
+    for i in range(1, n_qubits):
+        if i == qubit_1 or i == qubit_2:
+            gate = torch.kron(gate, Y)
+        else:
+            gate = torch.kron(gate, I)
+    # R_yy gate matrix
+    R_yy = torch.matrix_exp(-1j * angle / 2 * gate)
+    return R_yy
+
+
+def create_x_matrix(n_qubits, qubit) -> torch.Tensor:
+    # Check if qubit indices are within the range
+    if qubit >= n_qubits or qubit < 0:
+        raise ValueError("Qubit indices are out of bounds.")
+    # Pauli X gate
+    X = torch.tensor([[0, 1], [1, 0]], dtype=torch.complex128)
+    # Identity matrix for other qubits
+    I = torch.eye(2, dtype=torch.complex128)
+    # Create the full tensor product for the x gate
+    if qubit == 0:
+        gate = X
+    else:
+        gate = I
+    for i in range(1, n_qubits):
+        if i == qubit:
+            gate = torch.kron(gate, X)
+        else:
+            gate = torch.kron(gate, I)
+    return gate
 
 
 class CP_QAOA:
@@ -106,80 +196,55 @@ class CP_QAOA:
             sample_counts = Counter(samples)
             # Convert counts to probabilities
             self.counts = {key: count / self.N_samples for key, count in sample_counts.items()}
-        """return np.mean([probability * qubo_cost(state=string_to_array(bitstring), QUBO_matrix=self.Q) for
-                        bitstring, probability in self.counts.items()])"""
-        H_c = np.array(Operator(get_qiskit_H(Q=self.Q)))
-        state_vector = np.array(execute(circuit, self.simulator).result().get_statevector()).flatten()
-        return float(np.real(np.dot(state_vector.conj(), np.dot(H_c, state_vector))))
+        return np.mean([probability * qubo_cost(state=string_to_array(bitstring), QUBO_matrix=self.Q) for
+                        bitstring, probability in self.counts.items()])
+        #H_c = np.array(Operator(get_qiskit_H(Q=self.Q)))
+        #state_vector = np.array(execute(circuit, self.simulator).result().get_statevector()).flatten()
+        #return float(np.real(np.dot(state_vector.conj(), np.dot(H_c, state_vector))))
 
     def get_gradient(self, angles) -> np.ndarray:
         """ Using parameter shift rule to calculate exact derivatives"""
-
-        params = [Parameter(f'theta_{i}') for i in range(len(angles))]
-        #__angles__ = iter(params)
         torch_angles = torch.tensor(angles,
-                                       dtype=torch.cfloat,
-                                       requires_grad=True)
+                                    requires_grad=True)
 
         # Defining circuit
-        #qcircuit = QuantumCircuit(self.n_qubits)
-        qcircuit = TorchQcircuit(self.n_qubits)
 
-        psi_0 = torch.tensor(np.array([1.0] + [0.0 for _ in range(2 ** self.n_qubits - 1)]),
-                                             dtype=torch.cfloat,requires_grad=True)
+        psi_0 = torch.tensor([1.0] + [0.0 for _ in range(2 ** self.n_qubits - 1)],
+                             dtype=torch.complex128,
+                             requires_grad=True)
 
         # Setting 'k' qubits to |1>
         for qubit_index in self.initialization_strategy:
-            #qcircuit.x(qubit_index)
-            qcircuit.add_x(qubit_index)
+            X_i = create_x_matrix(n_qubits=self.n_qubits, qubit=qubit_index)
+            psi_0 = torch.matmul(X_i, psi_0)
 
         counter = 0
         for layer in range(self.layers):
             # XX+YY terms
             for (qubit_i, qubit_j) in self.qubit_indices:
                 theta_ij = torch_angles[counter]
-                #qcircuit.rxx(theta=theta_ij, qubit1=qubit_i, qubit2=qubit_j)
-                #qcircuit.ryy(theta=theta_ij, qubit1=qubit_i, qubit2=qubit_j)
-                qcircuit.add_rxx(angle=theta_ij, qubit_1=qubit_i, qubit_2=qubit_j)
-                qcircuit.add_ryy(angle=theta_ij, qubit_1=qubit_i, qubit_2=qubit_j)
+                rxx = create_Rxx_matrix(n_qubits=self.n_qubits, qubit_1=qubit_i, qubit_2=qubit_j, angle=theta_ij)
+                ryy = create_Ryy_matrix(n_qubits=self.n_qubits, qubit_1=qubit_i, qubit_2=qubit_j, angle=theta_ij)
+                psi_0 = torch.matmul(ryy, torch.matmul(rxx, psi_0))
                 counter += 1
 
             # Z terms
             if self.with_z_phase:
                 for qubit_i in range(self.n_qubits):
-                    #qcircuit.rz(phi=next(__angles__), qubit=qubit_i)
                     theta_i = torch_angles[counter]
-                    qcircuit.add_rz(angle=theta_i, target_qubit=qubit_i)
+                    rz = create_Rz_matrix(n_qubits=self.n_qubits, qubit=qubit_i, angle=theta_i)
+                    psi_0 = torch.matmul(rz,psi_0)
                     counter += 1
 
         # Get cost hamiltonian
-        #H_c = get_qiskit_H(Q=self.Q)
         H_c = torch.tensor(np.array(Operator(get_qiskit_H(Q=self.Q))),
-                           dtype=torch.cfloat,
+                           dtype=torch.complex128,
                            requires_grad=True)
-        U_theta = qcircuit.get_circuit_unitary()
-        psi_final = torch.matmul(U_theta, psi_0)
-        cost = torch.real(torch.dot(torch.conj(psi_final), torch.matmul(H_c, psi_final)))
-
-        # Backpropagation to calculate gradients
-        cost.backward()
-
-        print(torch_angles.grad, psi_0.grad)
+        c = torch.real(torch.dot(torch.conj(psi_0), torch.matmul(H_c, psi_0)))
+        c.backward()
 
         # Extracting and returning the gradient as a numpy array
         angle_gradients = torch_angles.grad.numpy()
-
-        """# Parameter values list (multiplying w. 2 as values in set circuit = 2*angle)
-        param_values = [[theta for theta in angles]]
-
-        # Define the gradient
-        gradient = FiniteDiffEstimatorGradient(Estimator(), epsilon=0.001)
-
-        # Evaluate the gradient of the circuits using parameter shift gradients
-        pse_grad_result = gradient.run(circuits=[qcircuit],
-                                       observables=[H_c],
-                                       parameters=[params],
-                                       parameter_values=param_values).result().gradients"""
 
         return angle_gradients
 

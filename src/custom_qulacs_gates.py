ab20a412f80e663edd0a71a6004b2524eef7963c
Sebastian Yde Madsen <madsen3008@gmail.com>
2024-02-12 16:11:55 +0100

    gerg

---
+++ src/custom_qulacs_gates.py
@@ -0,0 +1,22 @@
+from qulacs.gate import H, CNOT, RZ, RX
+import numpy as np
+
+
+def RXX(circuit, angle, qubit_1, qubit_2):
+    circuit.add_gate(H(index=qubit_1))
+    circuit.add_gate(H(index=qubit_2))
+    circuit.add_gate(CNOT(control=qubit_1, target=qubit_2))
+    circuit.append(RZ(index=qubit_2, angle=angle))
+    circuit.add_gate(CNOT(control=qubit_1, target=qubit_2))
+    circuit.add_gate(H(index=qubit_1))
+    circuit.add_gate(H(index=qubit_2))
+
+
+def RYY(circuit, angle, qubit_1, qubit_2):
+    circuit.add_gate(RX(index=qubit_1, angle=np.pi/2))
+    circuit.add_gate(RX(index=qubit_2, angle=np.pi/2))
+    circuit.add_gate(CNOT(control=qubit_1, target=qubit_2))
+    circuit.append(RZ(index=qubit_2, angle=angle))
+    circuit.add_gate(CNOT(control=qubit_1, target=qubit_2))
+    circuit.add_gate(RX(index=qubit_1, angle=-np.pi / 2))
+    circuit.add_gate(RX(index=qubit_2, angle=-np.pi / 2))
